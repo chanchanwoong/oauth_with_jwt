f483d60e98ed0a2574a48964c806f066dea02985
chanchanwoong <cksdnddlek99@gmail.com>
2024-03-18 21:19:11 +0900

    Add: 프로젝트 셋팅

---
+++ oauthandjwt/src/test/java/com/chanwoong/oauthandjwt/OauthandjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanwoong.oauthandjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthandjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
