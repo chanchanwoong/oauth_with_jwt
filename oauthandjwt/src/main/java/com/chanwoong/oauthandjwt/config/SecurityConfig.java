ad8351ab0fbb652abff7ac726da0dbb414d62196
chanchanwoong <cksdnddlek99@gmail.com>
2024-03-19 21:19:20 +0900

    Add: SecurityConfig 필터 위치 수정

--- oauthandjwt/src/main/java/com/chanwoong/oauthandjwt/config/SecurityConfig.java
+++ oauthandjwt/src/main/java/com/chanwoong/oauthandjwt/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -80,7 +81,7 @@ public class SecurityConfig {
 
         // JWTFilter 등록
         http
-                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAfter(new JWTFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
 
 
 
